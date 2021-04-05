ffa0567a601b2a1f3200132e2ad26edce9d1320e
Alejo Boga <alejoboga20@gmail.com>
2021-04-05 11:54:35 -0300

    Adding Default Props Value

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const App = ({ title }) => {
+const App = ({ title, subtitle }) => {
   return (
     <>
       <h1>{title}</h1>
-      <p>First App</p>
+      <p>{subtitle}</p>
     </>
   );
 };
 
 App.propTypes = {
   title: PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+  subtitle: 'First App using React JS'
 };
 export default App;
