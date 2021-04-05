6889d43d7e745b3ab5bf8976318b8be2e98cac89
Alejo Boga <alejoboga20@gmail.com>
2021-04-05 11:09:06 -0300

    Using fragment as wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>Counter React App</h1>;
+  return (
+    <>
+      <h1>Counter React App</h1>
+      <p>First App</p>
+    </>
+  );
 };
 
 export default App;
